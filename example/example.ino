809d232b31dd12a54ccf864bf720fa93d1034548
c-math <christer@fastmail.net>
2015-05-07 15:50:36 +0200

    Single pulse interface

--- example/example.ino
+++ example/example.ino
@@ -5,7 +5,9 @@ PrologueDecoder decoder;
 RF_Switch rfSwitch(2);
 
 int pw;
+bool high;
 Data data;
+
 void setup(){
 	pinMode(2,INPUT);
 	Serial.begin(115200);
@@ -13,12 +15,9 @@ void setup(){
 }
 
 void loop(){
-	if ((pw = rfSwitch.highPulse())){
-		decoder.pulse(pw, 1);
-	}
 
-	if ((pw = rfSwitch.lowPulse())){
-		decoder.pulse(pw, 0);
+	if ((pw = rfSwitch.pulse(&high))){
+		decoder.pulse(pw, high);
 	}
 
 	if (decoder.hasDetected()){
