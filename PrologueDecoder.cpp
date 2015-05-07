9a38009397af462352b4ad95bb4345f1e353d4a1
c-math <christer@fastmail.net>
2015-05-07 22:32:00 +0200

    Minor cleanup

--- PrologueDecoder.cpp
+++ PrologueDecoder.cpp
@@ -15,54 +15,53 @@ void PrologueDecoder::pulse(word width, bool high){
 	switch (state){
 	
 	case IDLE:
-		{
-			if (high && ( (width > 330) && (width < 530))){
-				state = SYNCING;
-			}
 
-			break;
+		if (high && (width > 330 && width < 530)){
+			state = SYNCING;
 		}
+
+		break;
+
 	case SYNCING:
-		{
-			if (!high && ( (width > 7500) && (width < 10000))){
-				state = SYNCED;
 
-			} else {
-				reset();
-			}
+		if (!high && (width > 7500 && width < 10000)){
+			state = SYNCED;
 
-			break;
+		} else {
+			reset();
 		}
+
+		break;
+		
 	case SYNCED:
-		{
-			if (high){
 
-				if  ((width < 330) || (width > 530)){
-					reset();
-				}
+		if (high){
 
-			} else {
-				
-				if ((width > 1500) && (width < 2500)){
-					bits[bitN++] = 0;
-
-				} else if ((width > 3500) && (width < 4500)){
-					bits[bitN++] = 1;
-
-				}else{
-					reset();
-				}
-
-			}
-
-			if (bitN == 36){
-				detected = true;
-				decode();
+			if  (width < 330 || width > 530){
 				reset();
 			}
 
-			break;
+		} else {
+			
+			if (width > 1500 && width < 2500){
+				bits[bitN++] = 0;
+
+			} else if (width > 3500 && width < 4500){
+				bits[bitN++] = 1;
+
+			}else{
+				reset();
+			}
+
 		}
+
+		if (bitN == 36){
+			detected = true;
+			decode();
+			reset();
+		}
+
+		break;
 	}
 }
 
