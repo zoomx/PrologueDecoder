8e865c430b6d6e59bcca0654332c9929a6e9105f
zoomx <zoomx@tiscalinet.it>
2018-01-26 11:26:28 +0100

    Added example for Digoo
    Added example for Digoo outdoor temperature humidity sensor R8S

---
+++ example/Prologue.ino
@@ -0,0 +1,42 @@
+/*
+  Prologue
+  by c-math
+  RF protocol decoder for the Prologue temperature sensor
+  https://github.com/c-math/PrologueDecoder
+
+  Added humidity decoding by zoomx
+  2017/12/02
+*/
+
+
+
+#include "RF_Switch.h"
+#include "PrologueDecoder.h"
+
+PrologueDecoder decoder;
+RF_Switch rfSwitch(2);
+
+int pw;
+bool high;
+PrologueData data;
+
+void setup() {
+  pinMode(2, INPUT);
+  Serial.begin(115200);
+  Serial.println("Prologue");
+}
+
+void loop() {
+  if ((pw = rfSwitch.pulse(&high))) {
+    if (decoder.pulse(pw, high)) {
+      data = decoder.getData();
+      Serial.println(data.temp);
+      Serial.println(data.humidity);
+      Serial.println(data.ID);
+      Serial.println(data.rollingID);
+      Serial.println(data.battery);
+      Serial.println(data.button);
+    }
+  }
+
+}
